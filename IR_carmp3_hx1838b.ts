8be2030bf9cf5b3dbec6e5330778242d8fca865f
iqfu321 <iqfu321@163.com>
2022-01-30 22:24:50 +0800

    更新 main.blocks, IR_carmp3_hx1838b.ts

--- IR_carmp3_hx1838b.ts
+++ IR_carmp3_hx1838b.ts
@@ -1,1 +1,238 @@
 // 在此处添加您的代码
+let c: number = 0;
+const enum IrButton {
+    CH_MINUS = 162,
+    CH = 98,
+    CH_Add = 226,
+    PREV = 34,
+    NEXT = 2,
+    PLAY = 194,
+    Minus = 224,
+    Add = 168,
+    EQ = 144,
+    NUM0 = 104,
+    NUM_100 = 152,
+    NUM_200 = 176,
+    NUM1 = 48,
+    NUM2 = 24,
+    NUM3 = 122,
+    NUM4 = 16,
+    NUM5 = 56,
+    NUM6 = 90,
+    NUM7 = 66,
+    NUM8 = 74,
+    NUM9 = 82,
+    Any = -1
+}
+
+const enum IrButtonAction {
+    //% block="pressed"
+    Pressed = 0,
+    //% block="released"
+    Released = 1,
+}
+namespace IR_carmp3_hx1838b {
+    let irState: IrState;
+
+    const MICROBIT_IR_NEC = 777;
+    const MICROBIT_IR_DATAGRAM = 778;
+    const MICROBIT_IR_BUTTON_PRESSED_ID = 789;
+    const MICROBIT_IR_BUTTON_RELEASED_ID = 790;
+    const IR_REPEAT = 256;
+    const IR_INCOMPLETE = 257;
+    const IR_DATAGRAM = 258;
+
+    interface IrState {
+        hasNewDatagram: boolean;
+        bitsReceived: uint8;
+        addressSectionBits: uint16;
+        commandSectionBits: uint16;
+        hiword: uint16;
+        loword: uint16;
+    }
+
+    function appendBitToDatagram(bit: number): number {
+        irState.bitsReceived += 1;
+
+        if (irState.bitsReceived <= 8) {
+            irState.hiword = (irState.hiword << 1) + bit;
+        } else if (irState.bitsReceived <= 16) {
+            irState.hiword = (irState.hiword << 1) + bit;
+        } else if (irState.bitsReceived <= 32) {
+            irState.loword = (irState.loword << 1) + bit;
+        }
+
+        if (irState.bitsReceived === 32) {
+            irState.addressSectionBits = irState.hiword & 0xffff;
+            irState.commandSectionBits = irState.loword & 0xffff;
+            return IR_DATAGRAM;
+        } else {
+            return IR_INCOMPLETE;
+        }
+    }
+
+    function decode(markAndSpace: number): number {
+        if (markAndSpace < 1600) {
+            // low bit
+            return appendBitToDatagram(0);
+        } else if (markAndSpace < 2700) {
+            // high bit
+            return appendBitToDatagram(1);
+        }
+
+        irState.bitsReceived = 0;
+
+        if (markAndSpace < 12500) {
+            // Repeat detected
+            return IR_REPEAT;
+        } else if (markAndSpace < 14500) {
+            // Start detected
+            return IR_INCOMPLETE;
+        } else {
+            return IR_INCOMPLETE;
+        }
+    }
+
+    function enableIrMarkSpaceDetection(pin: DigitalPin) {
+        pins.setPull(pin, PinPullMode.PullNone);
+
+        let mark = 0;
+        let space = 0;
+
+        pins.onPulsed(pin, PulseValue.Low, () => {
+            // HIGH, see https://github.com/microsoft/pxt-microbit/issues/1416
+            mark = pins.pulseDuration();
+        });
+
+        pins.onPulsed(pin, PulseValue.High, () => {
+            // LOW
+            space = pins.pulseDuration();
+
+            const status = decode(mark + space);
+
+            if (status !== IR_INCOMPLETE) {
+                control.raiseEvent(MICROBIT_IR_NEC, status);
+            }
+        });
+    }
+    /**
+   * Connects to the IR receiver module at the specified pin.
+   * @param pin IR receiver pin, eg: DigitalPin.P0
+   */
+    //% blockId="infrared_connect_receiver"
+    //% block="connect IR receiver at pin %pin"
+    //% pin.fieldEditor="gridpicker"
+    //% pin.fieldOptions.columns=4
+    //% pin.fieldOptions.tooltips=0
+    //% weight=90
+    export function connectIrReceiver(
+        pin: DigitalPin,
+    ): void {
+        if (irState) {
+            return;
+        }
+
+        serial.redirectToUSB();
+
+        irState = {
+            bitsReceived: 0,
+            hasNewDatagram: false,
+            addressSectionBits: 0,
+            commandSectionBits: 0,
+            hiword: 0, // TODO replace with uint32
+            loword: 0,
+        };
+
+        enableIrMarkSpaceDetection(pin);
+
+        let activeCommand = -1;
+        let repeatTimeout = 0;
+        const REPEAT_TIMEOUT_MS = 120;
+
+        control.onEvent(
+            MICROBIT_IR_NEC,
+            EventBusValue.MICROBIT_EVT_ANY,
+            () => {
+
+                const irEvent = control.eventValue();
+
+                // Refresh repeat timer
+                if (irEvent === IR_DATAGRAM || irEvent === IR_REPEAT) {
+                    repeatTimeout = input.runningTime() + REPEAT_TIMEOUT_MS;
+                }
+                if (irEvent === IR_DATAGRAM) {
+
+                    irState.hasNewDatagram = true;
+                    control.raiseEvent(MICROBIT_IR_DATAGRAM, 0);
+
+                    const newCommand = irState.commandSectionBits >> 8;
+
+                    serial.writeValue("IR_DATAGRAM", newCommand);
+                    // Process a new command
+                    if (newCommand !== activeCommand) {
+                        if (activeCommand >= 0) {
+                            control.raiseEvent(
+                                MICROBIT_IR_BUTTON_RELEASED_ID,
+                                activeCommand
+                            );
+                        }
+
+                        activeCommand = newCommand;
+                        control.raiseEvent(
+                            MICROBIT_IR_BUTTON_PRESSED_ID,
+                            newCommand
+                        );
+                    }
+                }
+            }
+        );
+
+        control.inBackground(() => {
+            while (true) {
+                if (activeCommand === -1) {
+                    // sleep to save CPU cylces
+                    basic.pause(2 * REPEAT_TIMEOUT_MS);
+                } else {
+                    const now = input.runningTime();
+                    if (now > repeatTimeout) {
+                        // repeat timed out
+                        control.raiseEvent(
+                            MICROBIT_IR_BUTTON_RELEASED_ID,
+                            activeCommand
+                        );
+                        activeCommand = -1;
+                    } else {
+                        basic.pause(REPEAT_TIMEOUT_MS);
+                    }
+                }
+            }
+        });
+    }
+    /**
+     * Do something when a specific button is pressed or released on the remote control.
+     * @param button the button to be checked
+     * @param action the trigger action
+     * @param handler body code to run when the event is raised
+     */
+    //% blockId=infrared_on_ir_button
+    //% block="on IR button | %button | %action"
+    //% button.fieldEditor="gridpicker"
+    //% button.fieldOptions.columns=3
+    //% button.fieldOptions.tooltips="false"
+    //% weight=100
+    export function onIrButton(
+        button: IrButton,
+        action: IrButtonAction,
+        handler: () => void
+    ) {
+        control.onEvent(
+            action === IrButtonAction.Pressed
+                ? MICROBIT_IR_BUTTON_PRESSED_ID
+                : MICROBIT_IR_BUTTON_RELEASED_ID,
+            button === IrButton.Any ? EventBusValue.MICROBIT_EVT_ANY : button,
+            () => {
+                handler();
+            }
+        );
+    }
+}
